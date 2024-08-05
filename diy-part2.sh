ba344f9c29dd35b322e6961ac65003f1b0082ee5
zow2023 <125123077+zow2023@users.noreply.github.com>
2024-08-05 12:19:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -24,6 +24,7 @@ git clone https://github.com/gSpotx2f/luci-app-temp-status package/luci-app-temp
 git clone https://github.com/muink/luci-app-tinyfilemanager package/luci-app-tinyfilemanager
 git clone https://github.com/muink/luci-app-tn-netports package/luci-app-tn-netports
 
+git clone https://github.com/QiuSimons/luci-app-daed package/dae
 
 #rm -rf feeds/packages/net/{xray-core,chinadns-ng,hysteria,v2ray-plugin}
 
