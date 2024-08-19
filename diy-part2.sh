96d0cfed6123620a0b21e174764df481887cda56
zow2023 <125123077+zow2023@users.noreply.github.com>
2024-08-19 20:26:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -52,4 +52,4 @@ sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-sed -i 's/OpenWrt/E8450/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/LINKJAY/g' package/base-files/files/bin/config_generate
