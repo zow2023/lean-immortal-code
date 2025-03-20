4e696e463755629fc71c128e9e06bb32cbb5099a
zow2023 <125123077+zow2023@users.noreply.github.com>
2025-03-20 13:37:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -24,28 +24,29 @@ git clone https://github.com/xiaoxiao29/luci-app-adguardhome package/luci-app-ad
 #git clone https://github.com/gSpotx2f/luci-app-cpu-perf package/luci-app-cpu-perf
 #git clone https://github.com/gSpotx2f/luci-app-interfaces-statistics package/luci-app-interfaces-statistics
 #git clone https://github.com/gSpotx2f/luci-app-temp-status package/luci-app-temp-status
-#git clone https://github.com/muink/luci-app-tinyfilemanager package/luci-app-tinyfilemanager
+git clone https://github.com/muink/luci-app-tinyfilemanager package/luci-app-tinyfilemanager
 #git clone https://github.com/muink/luci-app-tn-netports package/luci-app-tn-netports
 
  #rm -rf feeds/luci/applications/luci-app-dae
  ##rm -rf feeds/luci/applications/luci-app-daed
  #rm -rf feeds/packages/net/{dae,daed}
  #git clone https://github.com/QiuSimons/luci-app-daed package/dae
+ git clone https://github.com/x-wrt/com.x-wrt package/x
+ git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
 
-# rm -rf feeds/packages/net/{xray-core,sing-box,chinadns-ng,hysteria,v2ray-plugin}
-
-# rm -rf feeds/luci/applications/luci-app-passwall
-# git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall/packages
-# git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci  
+rm -rf feeds/packages/net/{xray-core,sing-box,chinadns-ng,hysteria,v2ray-plugin}
+rm -rf feeds/luci/applications/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall/packages
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci  
 #git clone https://github.com/xiaorouji/openwrt-passwall.git -b  luci-smartdns-dev package/passwall/luci
 
 #git clone https://github.com/fw876/helloworld.git -b master package/helloworld
 #rm -rf package/helloworld
 #git clone https://github.com/fw876/helloworld.git package/helloworld
 
-#git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-#git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-theme-argon-config
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-theme-argon-config
 # git clone https://github.com/muink/openwrt-einat-ebpf.git package/einat-ebpf
 # git clone https://github.com/muink/luci-app-einat.git package/luci-app-einat
 
@@ -53,8 +54,7 @@ rm -rf feeds/packages/lang/node
 git clone https://github.com/sbwml/feeds_packages_lang_node-prebuilt -b packages-23.05 feeds/packages/lang/node
 
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-#git clone https://github.com/kenzok8/golang -b 1.21 feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
@@ -63,4 +63,4 @@ sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-sed -i 's/OpenWrt/LINKJAY/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/LINKSYS/g' package/base-files/files/bin/config_generate
