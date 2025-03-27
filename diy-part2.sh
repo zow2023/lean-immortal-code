c7976bf0b6c1c08ac9bf63dffa41ad43a126058a
zow2023 <125123077+zow2023@users.noreply.github.com>
2025-03-27 13:32:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -55,7 +55,8 @@ git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-th
 # git clone https://github.com/muink/luci-app-einat.git package/luci-app-einat
 
 rm -rf feeds/packages/lang/node
-git clone https://github.com/sbwml/feeds_packages_lang_node-prebuilt -b packages-23.05 feeds/packages/lang/node
+#git clone https://github.com/sbwml/feeds_packages_lang_node-prebuilt -b packages-23.05 feeds/packages/lang/node
+git clone https://github.com/sbwml/feeds_packages_lang_node-prebuilt -b packages-24.10 feeds/packages/lang/node
 
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
