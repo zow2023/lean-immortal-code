0370645cb62738117ca092f3713a9f632dcb928b
zow2023 <125123077+zow2023@users.noreply.github.com>
2025-03-28 08:44:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -33,7 +33,7 @@ git clone https://github.com/muink/luci-app-tinyfilemanager package/luci-app-tin
  #rm -rf feeds/packages/net/{dae,daed}
  #git clone https://github.com/QiuSimons/luci-app-daed package/dae
  git clone https://github.com/x-wrt/com.x-wrt package/x
- git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+ #git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
 
 #rm -rf feeds/packages/net/{xray-core,sing-box,chinadns-ng,hysteria,v2ray-plugin}
