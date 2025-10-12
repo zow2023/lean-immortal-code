2d868067965a85472e063f57d9ce7a099af5784a
zow2023 <125123077+zow2023@users.noreply.github.com>
2025-10-12 21:55:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -37,8 +37,8 @@ git clone https://github.com/muink/luci-app-tn-netports package/luci-app-tn-netp
 #rm -rf feeds/luci/applications/luci-app-daed
 #rm -rf feeds/luci/applications/luci-app-homeproxy
 #rm -rf feeds/packages/net/{dae,daed}
-#git clone https://github.com/zow2023/luci-app-daed package/dae
-git clone https://github.com/QiuSimons/luci-app-daed package/dae
+git clone https://github.com/zow2023/luci-app-daed package/dae
+#git clone https://github.com/QiuSimons/luci-app-daed package/dae
 #git clone https://github.com/x-wrt/com.x-wrt package/x
 # git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
