cc3a4d1d0de5107a0e3a43cafec910decab65572
zow2023 <125123077+zow2023@users.noreply.github.com>
2026-01-02 00:26:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -42,7 +42,7 @@ git clone https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
 #git clone https://github.com/Pacalini/luci-app-dae package/dae
 #git clone https://github.com/zow2023/luci-app-daed package/dae
 #git clone https://github.com/QiuSimons/luci-app-daed package/dae
-#git clone https://github.com/zow2023/openwrt_helloworld package/helloworld
+git clone https://github.com/zow2023/openwrt_helloworld package/helloworld
 
 #git clone https://github.com/x-wrt/com.x-wrt package/x
 # git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
