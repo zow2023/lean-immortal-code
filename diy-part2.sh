3ae242134a722917ae81e58a053666a5af58392a
zow2023 <125123077+zow2023@users.noreply.github.com>
2026-01-17 14:55:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,28 +15,28 @@
 
 #git clone https://github.com/4IceG/luci-app-timecontrol package/luci-app-timecontrol
 #git clone https://github.com/xiaoxiao29/luci-app-adguardhome package/luci-app-adguardhome
-#git clone https://github.com/TanZhiwen2001/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/TanZhiwen2001/luci-app-adguardhome package/luci-app-adguardhome
 #git clone https://github.com/sirpdboy/luci-app-adguardhome package/luci-app-adguardhome
-git clone https://github.com/w9315273/luci-app-adguardhome package/luci-app-adguardhome
+#git clone https://github.com/w9315273/luci-app-adguardhome package/luci-app-adguardhome
 
 # git clone https://github.com/sbwml/luci-app-filemanager package/luci-app-filemanager
 
 #git clone https://github.com/animegasan/luci-app-quickstart package/luci-app-quickstart
 ##git clone https://github.com/brvphoenix/luci-app-wrtbwmon package/luci-app-wrtbwmon
 
-#git clone https://github.com/gSpotx2f/luci-app-cpu-status package/luci-app-cpu-status
-#git clone https://github.com/gSpotx2f/luci-app-cpu-perf package/luci-app-cpu-perf
-#git clone https://github.com/gSpotx2f/luci-app-interfaces-statistics package/luci-app-interfaces-statistics
-#git clone https://github.com/gSpotx2f/luci-app-temp-status package/luci-app-temp-status
+git clone https://github.com/gSpotx2f/luci-app-cpu-status package/luci-app-cpu-status
+git clone https://github.com/gSpotx2f/luci-app-cpu-perf package/luci-app-cpu-perf
+git clone https://github.com/gSpotx2f/luci-app-interfaces-statistics package/luci-app-interfaces-statistics
+git clone https://github.com/gSpotx2f/luci-app-temp-status package/luci-app-temp-status
 #git clone https://github.com/muink/luci-app-tinyfilemanager package/luci-app-tinyfilemanager
-#git clone https://github.com/muink/luci-app-tn-netports package/luci-app-tn-netports
+git clone https://github.com/muink/luci-app-tn-netports package/luci-app-tn-netports
 
 rm -rf feeds/packages/net/{smartdns,xray-core,v2ray-core,chinadns-ng,v2ray-geodata,sing-box}
 
-rm -rf feeds/luci/applications/luci-app-dae
-rm -rf feeds/luci/applications/luci-app-daed
-rm -rf feeds/luci/applications/luci-app-homeproxy
-rm -rf feeds/packages/net/{dae,daed}
+#rm -rf feeds/luci/applications/luci-app-dae
+#rm -rf feeds/luci/applications/luci-app-daed
+#rm -rf feeds/luci/applications/luci-app-homeproxy
+#rm -rf feeds/packages/net/{dae,daed}
 
 #git clone https://github.com/douglarek/dae-openwrt package/dae
 
@@ -51,9 +51,9 @@ git clone https://github.com/zow2023/openwrt_helloworld package/helloworld
 
 rm -rf feeds/luci/applications/luci-app-smartdns
   
-#git clone https://github.com/zow2023/openwrt-smartdns feeds/packages/net/smartdns
+git clone https://github.com/zow2023/openwrt-smartdns feeds/packages/net/smartdns
 
-git clone https://github.com/pymumu/openwrt-smartdns feeds/packages/net/smartdns
+#git clone https://github.com/pymumu/openwrt-smartdns feeds/packages/net/smartdns
 git clone https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
 
 #rm -rf feeds/packages/net/{xray-core,sing-box,chinadns-ng,hysteria,v2ray-plugin}
@@ -74,14 +74,14 @@ git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-th
 # git clone https://github.com/muink/luci-app-einat.git package/luci-app-einat
 
 rm -rf feeds/packages/lang/node
-git clone https://github.com/sbwml/feeds_packages_lang_node-prebuilt -b packages-23.05 feeds/packages/lang/node
-#git clone https://github.com/sbwml/feeds_packages_lang_node-prebuilt #git clone https://github.com/sbwml/feeds_
+#git clone https://github.com/sbwml/feeds_packages_lang_node-prebuilt -b packages-23.05 feeds/packages/lang/node
+git clone https://github.com/sbwml/feeds_packages_lang_node-prebuilt -b packages-24.10 feeds/packages/lang/node
 
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.15.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
